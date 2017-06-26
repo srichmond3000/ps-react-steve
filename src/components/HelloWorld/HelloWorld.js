dc08aecc0948570b58a8e18efe91fc2588a5cb48
srichmond3000 <srichmond3000@googlemail.compwd>
2017-06-26 21:27:24 +0100

    Added doc generation scripts to package.json and a simple React component.

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HelloWorld = ({ message }) => {
+  return (
+    <div>
+      Hello {message}
+    </div>
+  );
+};
+
+HelloWorld.propTypes = {
+  message: PropTypes.string
+};
+
+export default HelloWorld;
